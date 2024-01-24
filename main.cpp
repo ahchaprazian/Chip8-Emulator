6c16b6880c5e0a0f25c8882779d015535cd40ec7
Alexan Chaprazian <ahchaprazian@gmail.com>
2024-01-23 21:03:11 -0500

    added loggers and started to implement sdl graphics

--- main.cpp
+++ main.cpp
@@ -1,43 +1,77 @@
 #include <iostream>
 
-//#include "/opt/homebrew/include/SDL2/SDL.h"
+#include <boost/log/trivial.hpp>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_render.h>
 #include "chip8.hpp"
 
 
 int main(int argc, char* argv[]) {
 
     if(argv[1] == NULL) {   // check if a rom was insterted if not print error and exit
-        fprintf(stderr, "ERROR: No rom was insterted\n");
-        exit(1);
+        std::cerr << "ERROR: ROM file was not inserted.\n";
+        BOOST_LOG_TRIVIAL(error) << "No ROM file was inserted.";
+        std::exit(EXIT_FAILURE);
     }
 
+    BOOST_LOG_TRIVIAL(info) << "Correct number of arguments passed in.";
+
     Chip8 chip8Emulator = Chip8();
-    chip8Emulator.loadROM(argv[1]);
-    uint16_t opcode = chip8Emulator.fetch();
-    chip8Emulator.decodeAndExecute(opcode);
+
+    if(!chip8Emulator.loadROM(argv[1])) {
+        std::cerr << "ERROR: ROM failed to load.\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+
+    //uint16_t opcode = chip8Emulator.fetch();
+    //chip8Emulator.decodeAndExecute(opcode);
     
 
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    SDL_Texture* texture;
+    const int height = 320, width = 640;
     // Initialize SDL
     if (SDL_Init(SDL_INIT_VIDEO) != 0) {
         SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
-        return 1;
+        //return 1;
+        std::cerr << "ERROR: failed to initialize SDL " << SDL_GetError() << "\n";
+        SDL_Quit();
+        std::exit(EXIT_FAILURE);
     }
 
     // Create a window
-    SDL_Window* window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "Chip-8 Emulator",
         SDL_WINDOWPOS_CENTERED,
         SDL_WINDOWPOS_CENTERED,
-        1024, 640,
+        width, height,
         SDL_WINDOW_SHOWN
     );
 
     if (window == nullptr) {
         SDL_Log("Failed to create window: %s", SDL_GetError());
-        return 1;
+        std::cerr << "ERROR: failed to create window " << SDL_GetError() << "\n";
+        std::exit(EXIT_FAILURE);
     }
 
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        SDL_Log("Failed to init rendering: %s", SDL_GetError());
+        std::cerr << "ERROR: failed to init rendering " << SDL_GetError() << "\n";
+        std::exit(EXIT_FAILURE);
+    }
+
+    SDL_RenderSetLogicalSize(renderer, width, height);
+
+    texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888, SDL_TEXTUREACCESS_STREAMING, 64, 32);
+    if (texture == nullptr) {
+        SDL_Log("Failed to init texture: %s", SDL_GetError());
+        std::cerr << "ERROR: failed to init texture " << SDL_GetError() << "\n";
+        std::exit(EXIT_FAILURE);
+    }
 
     // if (renderer == nullptr) {
     //     SDL_Log("Failed to create renderer: %s", SDL_GetError());
